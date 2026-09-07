6a36f5f93889da1c411e4d7270dbad8a8e75bfa9
cesar822 <costacesar055@gmail.com>
2026-09-07 13:59:35 -0300

    fix(notification): corrige bug no postgres e adiciona depência faltante do pom.xml

--- src/main/java/com/notification/notification_service/entity/NotificationEntity.java
+++ src/main/java/com/notification/notification_service/entity/NotificationEntity.java
@@ -18,11 +18,13 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "tb_notifications")
 public class NotificationEntity {
 
--- src/main/java/com/notification/notification_service/producer/NotificationProducer.java
+++ src/main/java/com/notification/notification_service/producer/NotificationProducer.java
@@ -5,8 +5,6 @@ import java.util.UUID;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import com.notification.notification_service.entity.NotificationEntity;
-import com.notification.notification_service.repository.NotificationRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 public class NotificationProducer {
 
     private final RabbitTemplate rabbitTemplate;
-    private final NotificationRepository notificationRepository;
 
     @Value("${notification.exchanges.main}")
     private String mainExchangeName;
@@ -24,9 +21,6 @@ public class NotificationProducer {
     private String emailRoutingKey;
 
     public void publishNotification(UUID notificationUUID) {
-        NotificationEntity notificationEntity = notificationRepository.findById(notificationUUID)
-                .orElseThrow(() -> new IllegalArgumentException(
-                        "Notification not found for ID: " + notificationUUID));
-        rabbitTemplate.convertAndSend(mainExchangeName, emailRoutingKey, notificationEntity);
+        rabbitTemplate.convertAndSend(mainExchangeName, emailRoutingKey, notificationUUID);
     }
 }
